5b58d410adc705b9fb656e99f39d8f2fd72982ac
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 22:50:18 +0530

    feat : - adding DOM Assignment

---
+++ JavaScript-Assignment/variable.js
@@ -0,0 +1,11 @@
+let stringName = 'Ashish'
+console.log(typeof stringName)
+
+let variable = true
+console.log(typeof variable)
+
+let variable1 
+ console.log(typeof variable1)
+
+ let variableName = null
+ console.log( typeof variableName) //"object" (not "null" for legacy reasons)
