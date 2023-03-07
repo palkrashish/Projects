c9ee2ea8513e7a5b55b81fccc88ae1acd8d3bce7
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:19:33 +0530

    add: - que4 includes

---
+++ JavaScript-Assignment/que4.js
@@ -0,0 +1,5 @@
+// 04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method.
+
+const nameFriends = ['Rohan', 'Sohan', 'Rahul', 'Tushar', 'Jai', 'RamMohan', 'TejSingh']
+
+console.log(nameFriends.includes('Rohan'))
