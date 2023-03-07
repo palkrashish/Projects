0bfa2178d8fa140b5287f375a957faeb1411f7f4
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:16:10 +0530

    add:- toUpperCase() que3

---
+++ JavaScript-Assignment/que3.js
@@ -0,0 +1,5 @@
+// 03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let stringName = "ashish kr pal"
+
+ console.log(stringName.toUpperCase())
