01be12f52851997460ab20780d60852917ae07f5
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:23:28 +0530

    add: split que5

---
+++ JavaScript-Assignment/que5.js
@@ -0,0 +1,5 @@
+// 05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+const stringName = 'I am Ashish Kr Pal. I joined a Course Full Stack JavaScript Developer Course '
+
+console.log(stringName.split(''))
