4d95a3e74954d69446a1d0b3e3dc4028059c55c8
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:51:10 +0530

    add: que 7 firstIndexOf

---
+++ JavaScript-Assignment/que7.js
@@ -0,0 +1,7 @@
+// 07. Declare an array containing the multiple values and use lastIndexOf to determine the position of the first and last occurrence of a word "pw skills".
+
+const Array = ['FullStack JavaScript', 'DataScience', 'pw skills', 'Docker', 'Devpos', 'Angular', 'C++', 'pw skills', 'Java'];
+
+
+console.log(`My first occurrence of a pw skills ${Array.indexOf('pw skills')}`)
+console.log(`My lastIndexOf occurrence of a pw skill ${Array.lastIndexOf('pw skills')}`)
