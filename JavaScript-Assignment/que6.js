8dcc2f6336d971b73af329ab80bc957426dd1f92
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:39:20 +0530

    add:  Que6 split

---
+++ JavaScript-Assignment/que6.js
@@ -0,0 +1,6 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+const companyName = 'facebook , Google, Microsoft, Apple, IBM , Oracle, Amazon'
+
+
+console.log(companyName.split(','))
