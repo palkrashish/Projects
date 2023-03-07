9ba4ed06f64b4de7b3b7d88ded2b09d6137bb7f9
Ashish Kr Pal <74855648+palkrashish@users.noreply.github.com>
2023-03-07 23:06:21 +0530

    add: - solution Que 2

---
+++ JavaScript-Assignment/que2.js
@@ -0,0 +1,9 @@
+// 02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = 'Ashish'
+let lastName = 'Pal'
+let maritalStatus = 'Unmarried'
+let  country = 'India'
+let age = 28
+
+console.log(`Hello My Name is ${firstName} ${lastName}. I am ${maritalStatus}, Currently living in ${country} and age is ${age}. `)
